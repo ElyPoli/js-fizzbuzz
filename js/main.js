fc9726d771ace0252a2af98d0483ca2e95f9fec0
ElyPoli <itsos.poli@gmail.com>
2023-07-04 14:15:24 +0200

    added js code

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
 **Prima di partire a scrivere codice poniamoci qualche domanda:**
 Come faccio a sapere se un numero è divisibile per un altro?
 Abbiamo visto qualcosa di particolare che possiamo usare?
@@ -19,3 +20,18 @@ Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda 
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
 */
 
+// Ciclo per stampare i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+
+    // Verifico i numeri multipli di 3 o 5 oppure di entrambi
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
+    }
+
+}
